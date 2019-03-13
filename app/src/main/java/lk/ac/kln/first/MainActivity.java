47adf7d39a4e0aef8ff15a7085f3165615095a00
DayaKathir <dayakathir@gmail.com>
2019-03-13 14:30:21 +0530

    Start / stop button

--- app/src/main/java/lk/ac/kln/first/MainActivity.java
+++ app/src/main/java/lk/ac/kln/first/MainActivity.java
@@ -3,15 +3,30 @@ package lk.ac.kln.first;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private int counter = 99;
+    private int counter = 100;
+    private  boolean running=false;
+    private boolean wasRunning=false;
+    private  static  final String CURRENT_COUNTER="counter";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState!=null){
+            counter=savedInstanceState.getInt(CURRENT_COUNTER);
+        }
+        //running=true;
         countdown();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle SavedInstanceState){
+        super.onSaveInstanceState(SavedInstanceState);
+       // savedInstanceState.putInt(CURRENT_COUNTER,counter);
     }
 
     private void countdown() {
@@ -20,13 +35,46 @@ public class MainActivity extends AppCompatActivity {
         handler.post(new Runnable() {
             @Override
             public void run() {
+                //reset the counter
+                if(counter==0){
+                    counter=99;
+                }
                 textview.setText(Integer.toString(counter));
-                counter--;
+
+                if (running){
+                    counter--;
+                }
+
                 handler.postDelayed(this, 1000);
             }
         });
 
 
+    }
+
+    protected void startCounter(View view){
+        running = true;
+    }
+
+    public void stopCounter(View view) {
+        running = false;
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        running = true;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        wasRunning = true;
     }
 
 
